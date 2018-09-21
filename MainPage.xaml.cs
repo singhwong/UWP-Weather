fbe5d6e92432431e0c89302090b1c12302c3e12f
singhwong <singhwongwxg@hotmail.com>
2018-09-21 13:15:43 +0800

    add refresh button

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -54,6 +54,7 @@ namespace UsefulWeather
                 itemone_bool = true;
                 itemtwo_bool = false;
                 itemthree_bool = false;
+                refresh_button.Visibility = Visibility.Visible;
             }
             else if (listbox_itemtwo.IsSelected)
             {
@@ -66,6 +67,7 @@ namespace UsefulWeather
                 itemone_bool = false;
                 itemtwo_bool = true;
                 itemthree_bool = false;
+                refresh_button.Visibility = Visibility.Collapsed;
             }
             else if(listbox_itemthree.IsSelected)
             {
@@ -78,64 +80,79 @@ namespace UsefulWeather
                 itemone_bool = false;
                 itemtwo_bool = false;
                 itemthree_bool = true;
-            }
-       
+                refresh_button.Visibility = Visibility.Collapsed;
+            }       
         }
 
         private void Page_Loaded(object sender, RoutedEventArgs e)
         {
             setting_stackPanel.Visibility = Visibility.Collapsed;
             back_button.Visibility = Visibility.Collapsed;
+            itemone_bool = true;
         }
 
         private void setting_combobox_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
+            SolidColorBrush red = new SolidColorBrush(Colors.Red);
+            SolidColorBrush skyBlue = new SolidColorBrush(Colors.SkyBlue);
+            SolidColorBrush black = new SolidColorBrush(Colors.Black);
+            SolidColorBrush gray = new SolidColorBrush(Colors.Gray);
+            SolidColorBrush lightGray = new SolidColorBrush(Colors.LightGray);
+            SolidColorBrush whiteSmoke = new SolidColorBrush(Colors.WhiteSmoke);
+            SolidColorBrush deepPink = new SolidColorBrush(Colors.DeepPink);
             var value = (ComboBox)sender;
             var selectionvalue = (ComboBoxItem)value.SelectedItem;
             var color_value = selectionvalue.Content;
             switch (color_value)
             {
-                case "Red": this.main_grid.Background = new SolidColorBrush(Colors.Red);
-                    this.main_listbox.Background = new SolidColorBrush(Colors.Red);
-                    this.setting_listbox.Background = new SolidColorBrush(Colors.Red);
-                    this.list_button.Background = new SolidColorBrush(Colors.Red);
-                    this.back_button.Background = new SolidColorBrush(Colors.Red);
+                case "Red": this.main_grid.Background = red;
+                    this.main_listbox.Background = red;
+                    this.setting_listbox.Background = red;
+                    this.list_button.Background = red;
+                    this.back_button.Background = red;
+                    this.refresh_button.Background = red;
                     break;
-                case "SkyBlue": this.main_grid.Background = new SolidColorBrush(Colors.SkyBlue);
-                    this.main_listbox.Background = new SolidColorBrush(Colors.SkyBlue);
-                    this.setting_listbox.Background = new SolidColorBrush(Colors.SkyBlue);
-                    this.list_button.Background = new SolidColorBrush(Colors.SkyBlue);
-                    this.back_button.Background = new SolidColorBrush(Colors.SkyBlue);
+                case "SkyBlue": this.main_grid.Background = skyBlue;
+                    this.main_listbox.Background = skyBlue;
+                    this.setting_listbox.Background = skyBlue;
+                    this.list_button.Background = skyBlue;
+                    this.back_button.Background = skyBlue;
+                    this.refresh_button.Background = skyBlue;
                     break;
-                case "Black": this.main_grid.Background = new SolidColorBrush(Colors.Black);
-                    this.main_listbox.Background = new SolidColorBrush(Colors.Black);
-                    this.setting_listbox.Background = new SolidColorBrush(Colors.Black);
-                    this.list_button.Background = new SolidColorBrush(Colors.Black);
-                    this.back_button.Background = new SolidColorBrush(Colors.Black);
+                case "Black": this.main_grid.Background = black;
+                    this.main_listbox.Background = black;
+                    this.setting_listbox.Background = black;
+                    this.list_button.Background = black;
+                    this.back_button.Background = black;
+                    this.refresh_button.Background = black;
                     break;
-                case "Gray": this.main_grid.Background = new SolidColorBrush(Colors.Gray);
-                    this.main_listbox.Background = new SolidColorBrush(Colors.Gray);
-                    this.setting_listbox.Background = new SolidColorBrush(Colors.Gray);
-                    this.list_button.Background = new SolidColorBrush(Colors.Gray);
-                    this.back_button.Background = new SolidColorBrush(Colors.Gray);
+                case "Gray": this.main_grid.Background = gray;
+                    this.main_listbox.Background = gray;
+                    this.setting_listbox.Background = gray;
+                    this.list_button.Background = gray;
+                    this.back_button.Background = gray;
+                    this.refresh_button.Background = gray;
                     break;
-                case "LightGray": this.main_grid.Background = new SolidColorBrush(Colors.LightGray);
-                    this.main_listbox.Background = new SolidColorBrush(Colors.LightGray);
-                    this.setting_listbox.Background = new SolidColorBrush(Colors.LightGray);
-                    this.list_button.Background = new SolidColorBrush(Colors.LightGray);
-                    this.back_button.Background = new SolidColorBrush(Colors.LightGray);
+                case "LightGray": this.main_grid.Background = lightGray;
+                    this.main_listbox.Background = lightGray;
+                    this.setting_listbox.Background = lightGray;
+                    this.list_button.Background = lightGray;
+                    this.back_button.Background = lightGray;
+                    this.refresh_button.Background = lightGray;
                     break;
-                case "WhiteSmoke": this.main_grid.Background = new SolidColorBrush(Colors.WhiteSmoke);
-                    this.main_listbox.Background = new SolidColorBrush(Colors.WhiteSmoke);
-                    this.setting_listbox.Background = new SolidColorBrush(Colors.WhiteSmoke);
-                    this.list_button.Background = new SolidColorBrush(Colors.WhiteSmoke);
-                    this.back_button.Background = new SolidColorBrush(Colors.WhiteSmoke);
+                case "WhiteSmoke": this.main_grid.Background = whiteSmoke;
+                    this.main_listbox.Background = whiteSmoke;
+                    this.setting_listbox.Background = whiteSmoke;
+                    this.list_button.Background = whiteSmoke;
+                    this.back_button.Background = whiteSmoke;
+                    this.refresh_button.Background = whiteSmoke;
                     break;
-                case "DeepPink": this.main_grid.Background = new SolidColorBrush(Colors.DeepPink);
-                    this.main_listbox.Background = new SolidColorBrush(Colors.DeepPink);
-                    this.setting_listbox.Background = new SolidColorBrush(Colors.DeepPink);                   
-                    this.list_button.Background = new SolidColorBrush(Colors.DeepPink);
-                    this.back_button.Background = new SolidColorBrush(Colors.DeepPink);
+                case "DeepPink": this.main_grid.Background = deepPink;
+                    this.main_listbox.Background = deepPink;
+                    this.setting_listbox.Background = deepPink;                   
+                    this.list_button.Background = deepPink;
+                    this.back_button.Background = deepPink;
+                    this.refresh_button.Background = deepPink;
                     break;
                 default:break;
             }           
@@ -151,6 +168,7 @@ namespace UsefulWeather
                 main_scrollViewer.Visibility = Visibility.Collapsed;
                 setting_stackPanel.Visibility = Visibility.Visible;
                 main_title.Text = "Setting";
+                refresh_button.Visibility = Visibility.Collapsed;
             }
         }
 
@@ -162,7 +180,8 @@ namespace UsefulWeather
             back_button.Visibility = Visibility.Collapsed;
             if (itemone_bool==true)
             {
-                main_title.Text = "CurrentWeather"; 
+                main_title.Text = "CurrentWeather";
+                refresh_button.Visibility = Visibility.Visible;
             }
             if (itemtwo_bool==true)
             {
@@ -176,6 +195,11 @@ namespace UsefulWeather
             {
                 main_title.Text = "CurrentWeather";
             }
+        }
+
+        private void refresh_button_Click(object sender, RoutedEventArgs e)
+        {
+            main_frame.Navigate(typeof(HomePage));
         }
     }
 }
