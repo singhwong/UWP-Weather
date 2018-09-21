bb1f9b11356e732e4ab9cd19a2c2fd439e227b40
singhwong <singhwongwxg@hotmail.com>
2018-09-21 16:20:15 +0800

    update sth

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -28,11 +28,25 @@ namespace UsefulWeather
         private bool itemone_bool = false;
         private bool itemtwo_bool = false;
         private bool itemthree_bool = false;
+        private SolidColorBrush red = new SolidColorBrush(Colors.Red);
+        private SolidColorBrush skyBlue = new SolidColorBrush(Colors.SkyBlue);
+        private SolidColorBrush black = new SolidColorBrush(Colors.Black);
+        private SolidColorBrush gray = new SolidColorBrush(Colors.Gray);
+        private SolidColorBrush lightGray = new SolidColorBrush(Colors.LightGray);
+        private SolidColorBrush whiteSmoke = new SolidColorBrush(Colors.WhiteSmoke);
+        private SolidColorBrush deepPink = new SolidColorBrush(Colors.DeepPink);
+        private SolidColorBrush antiqueWhite = new SolidColorBrush(Colors.AntiqueWhite);
+        private SolidColorBrush aqua = new SolidColorBrush(Colors.Aqua);
+        private SolidColorBrush azure = new SolidColorBrush(Colors.Azure);
+        private SolidColorBrush coral = new SolidColorBrush(Colors.Coral);
+        private SolidColorBrush brown = new SolidColorBrush(Colors.Brown);
+        private SolidColorBrush darkViolet = new SolidColorBrush(Colors.DarkViolet);
+        private SolidColorBrush gold = new SolidColorBrush(Colors.Gold);
         //private const string SettingTheme = "Theme";
         public MainPage()
         {
             this.InitializeComponent();
-                main_frame.Navigate(typeof(HomePage));
+            main_frame.Navigate(typeof(HomePage));
         }
 
         private void list_button_Click(object sender, RoutedEventArgs e)
@@ -42,7 +56,7 @@ namespace UsefulWeather
 
         private void main_listbox_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            
+
             if (listbox_itemone.IsSelected)
             {
                 setting_listbox.SelectedIndex = -1;
@@ -69,7 +83,7 @@ namespace UsefulWeather
                 itemthree_bool = false;
                 refresh_button.Visibility = Visibility.Collapsed;
             }
-            else if(listbox_itemthree.IsSelected)
+            else if (listbox_itemthree.IsSelected)
             {
                 setting_listbox.SelectedIndex = -1;
                 setting_stackPanel.Visibility = Visibility.Collapsed;
@@ -81,7 +95,7 @@ namespace UsefulWeather
                 itemtwo_bool = false;
                 itemthree_bool = true;
                 refresh_button.Visibility = Visibility.Collapsed;
-            }       
+            }
         }
 
         private void Page_Loaded(object sender, RoutedEventArgs e)
@@ -91,76 +105,9 @@ namespace UsefulWeather
             itemone_bool = true;
         }
 
-        private void setting_combobox_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-            SolidColorBrush red = new SolidColorBrush(Colors.Red);
-            SolidColorBrush skyBlue = new SolidColorBrush(Colors.SkyBlue);
-            SolidColorBrush black = new SolidColorBrush(Colors.Black);
-            SolidColorBrush gray = new SolidColorBrush(Colors.Gray);
-            SolidColorBrush lightGray = new SolidColorBrush(Colors.LightGray);
-            SolidColorBrush whiteSmoke = new SolidColorBrush(Colors.WhiteSmoke);
-            SolidColorBrush deepPink = new SolidColorBrush(Colors.DeepPink);
-            var value = (ComboBox)sender;
-            var selectionvalue = (ComboBoxItem)value.SelectedItem;
-            var color_value = selectionvalue.Content;
-            switch (color_value)
-            {
-                case "Red": this.main_grid.Background = red;
-                    this.main_listbox.Background = red;
-                    this.setting_listbox.Background = red;
-                    this.list_button.Background = red;
-                    this.back_button.Background = red;
-                    this.refresh_button.Background = red;
-                    break;
-                case "SkyBlue": this.main_grid.Background = skyBlue;
-                    this.main_listbox.Background = skyBlue;
-                    this.setting_listbox.Background = skyBlue;
-                    this.list_button.Background = skyBlue;
-                    this.back_button.Background = skyBlue;
-                    this.refresh_button.Background = skyBlue;
-                    break;
-                case "Black": this.main_grid.Background = black;
-                    this.main_listbox.Background = black;
-                    this.setting_listbox.Background = black;
-                    this.list_button.Background = black;
-                    this.back_button.Background = black;
-                    this.refresh_button.Background = black;
-                    break;
-                case "Gray": this.main_grid.Background = gray;
-                    this.main_listbox.Background = gray;
-                    this.setting_listbox.Background = gray;
-                    this.list_button.Background = gray;
-                    this.back_button.Background = gray;
-                    this.refresh_button.Background = gray;
-                    break;
-                case "LightGray": this.main_grid.Background = lightGray;
-                    this.main_listbox.Background = lightGray;
-                    this.setting_listbox.Background = lightGray;
-                    this.list_button.Background = lightGray;
-                    this.back_button.Background = lightGray;
-                    this.refresh_button.Background = lightGray;
-                    break;
-                case "WhiteSmoke": this.main_grid.Background = whiteSmoke;
-                    this.main_listbox.Background = whiteSmoke;
-                    this.setting_listbox.Background = whiteSmoke;
-                    this.list_button.Background = whiteSmoke;
-                    this.back_button.Background = whiteSmoke;
-                    this.refresh_button.Background = whiteSmoke;
-                    break;
-                case "DeepPink": this.main_grid.Background = deepPink;
-                    this.main_listbox.Background = deepPink;
-                    this.setting_listbox.Background = deepPink;                   
-                    this.list_button.Background = deepPink;
-                    this.back_button.Background = deepPink;
-                    this.refresh_button.Background = deepPink;
-                    break;
-                default:break;
-            }           
-        }
-
         private void setting_listbox_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            
+
             if (listbox_itemSetting.IsSelected)
             {
                 back_button.Visibility = Visibility.Visible;
@@ -178,16 +125,16 @@ namespace UsefulWeather
             setting_stackPanel.Visibility = Visibility.Collapsed;
             setting_listbox.SelectedIndex = -1;
             back_button.Visibility = Visibility.Collapsed;
-            if (itemone_bool==true)
+            if (itemone_bool == true)
             {
                 main_title.Text = "CurrentWeather";
                 refresh_button.Visibility = Visibility.Visible;
             }
-            if (itemtwo_bool==true)
+            if (itemtwo_bool == true)
             {
                 main_title.Text = "LocationWeather";
             }
-            if (itemthree_bool==true)
+            if (itemthree_bool == true)
             {
                 main_title.Text = "CityWeather";
             }
@@ -200,6 +147,83 @@ namespace UsefulWeather
         private void refresh_button_Click(object sender, RoutedEventArgs e)
         {
             main_frame.Navigate(typeof(HomePage));
+        }
+
+        private void backGround_combobox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var value = (ComboBox)sender;
+            var selectionvalue = (ComboBoxItem)value.SelectedItem;
+            var color_value = selectionvalue.Content;
+            switch (color_value)
+            {
+                case "Red":SetBackGround(red);break;
+                case "SkyBlue":SetBackGround(skyBlue);break;
+                case "Black":SetBackGround(black);break;
+                case "Gray":SetBackGround(gray);break;
+                case "LightGray":SetBackGround(lightGray);break;
+                case "WhiteSmoke":SetBackGround(whiteSmoke);break;
+                case "DeepPink":SetBackGround(deepPink);break;
+                case "antiqueWhite":SetBackGround(antiqueWhite);break;
+                case "aqua":SetBackGround(aqua);break;
+                case "azure":SetBackGround(azure);break;
+                case "coral":SetBackGround(coral);break;
+                case "brown":SetBackGround(brown);break;
+                case "darkViolet":SetBackGround(darkViolet);break;
+                case "gold":SetBackGround(gold);break;
+                default: break;
+            }
+        }
+
+        private void foreGround_combobox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var value = (ComboBox)sender;
+            var selectionvalue = (ComboBoxItem)value.SelectedItem;
+            var color_value = selectionvalue.Content;
+            switch (color_value)
+            {
+                case "Red":SetForeGround(red);break;
+                case "SkyBlue":SetForeGround(skyBlue);break;
+                case "Black":SetForeGround(black);break;
+                case "Gray":SetForeGround(gray);break;
+                case "LightGray":SetForeGround(lightGray);break;
+                case "WhiteSmoke":SetForeGround(whiteSmoke);break;
+                case "DeepPink":SetForeGround(deepPink);break;
+                case "antiqueWhite":SetForeGround(antiqueWhite);break;
+                case "aqua":SetForeGround(aqua);break;
+                case "azure":SetForeGround(azure);break;
+                case "coral":SetForeGround(coral);break;
+                case "brown":SetForeGround(brown);break;
+                case "darkViolet":SetForeGround(darkViolet);break;
+                case "gold":SetForeGround(gold);break;
+                default: break;
+            }
+        }
+
+        private void SetForeGround(SolidColorBrush color)
+        {
+            list_button.Foreground = color;
+            refresh_button.Foreground = color;
+            back_button.Foreground = color;
+            main_title.Foreground = color;
+            home_textblock.Foreground = color;
+            home_Text.Foreground = color;
+            location_textblock.Foreground = color;
+            location_Text.Foreground = color;
+            city_textblock.Foreground = color;
+            city_Text.Foreground = color;
+            setting_textblock.Foreground = color;
+            setting_Text.Foreground = color;
+            backGround_textblock.Foreground = color;
+            foreGround_textblock.Foreground = color;
+        }
+        private void SetBackGround(SolidColorBrush color)
+        {
+            main_grid.Background = color;
+            main_listbox.Background = color;
+            setting_listbox.Background = color;
+            list_button.Background = color;
+            back_button.Background = color;
+            refresh_button.Background = color;
         }
     }
 }
